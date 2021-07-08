5e919e0e779ed80d925ea2a64aaa04e774649042
quyanlu <quyanlu@meorient.com>
2021-07-08 11:47:08 +0800

    通用事件绑定函数

---
+++ Second/bindEvent.js
@@ -0,0 +1,17 @@
+//通用事件监听函数
+function bindEvent(elem, type, selector, fn) {
+	if (fn == null) {
+		fn = selector;
+		selector = null;
+	}
+	elem.addEventListener(type, event => {
+		const target = event.target;
+		if (selector) {
+			if (target.matches(selector)) {
+				fn.call(target, event);
+			}
+		} else {
+			fn.call(target, event);
+		}
+	});
+}
